67efd5a62e332b260aa1a21114ef3e7f813b47c9
Natalie Jackman <natalie.noel.jackman@icloud.com>
2020-03-07 10:53:53 -0600

    Initial Commit

---
+++ RegexExplorer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RegexExplorer
+//
+//  Created by Natalie Jackman on 3/7/20.
+//  Copyright © 2020 Natalie Jackman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RegexExplorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RegexExplorer
+//
+//  Created by Natalie Jackman on 3/7/20.
+//  Copyright © 2020 Natalie Jackman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
